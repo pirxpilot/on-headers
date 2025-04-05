887e5866e7aac57b5d80bf66582910774be9851f
Damian Krzeminski <pirxpilot@furkot.com>
2025-04-05 11:50:19 -0600

    modernize code
    use `node:test` for test and coverage

--- index.js
+++ index.js
@@ -4,14 +4,12 @@
  * MIT Licensed
  */
 
-'use strict'
-
 /**
  * Module exports.
  * @public
  */
 
-module.exports = onHeaders
+module.exports = onHeaders;
 
 /**
  * Create a replacement writeHead method.
@@ -21,28 +19,28 @@ module.exports = onHeaders
  * @private
  */
 
-function createWriteHead (prevWriteHead, listener) {
-  var fired = false
+function createWriteHead(prevWriteHead, listener) {
+  let fired = false;
 
   // return function with core name and argument list
-  return function writeHead (statusCode) {
+  return function writeHead(_statusCode) {
     // set headers from arguments
-    var args = setWriteHeadHeaders.apply(this, arguments)
+    const args = setWriteHeadHeaders.apply(this, arguments);
 
     // fire listener
     if (!fired) {
-      fired = true
-      listener.call(this)
+      fired = true;
+      listener.call(this);
 
       // pass-along an updated status code
       if (typeof args[0] === 'number' && this.statusCode !== args[0]) {
-        args[0] = this.statusCode
-        args.length = 1
+        args[0] = this.statusCode;
+        args.length = 1;
       }
     }
 
-    return prevWriteHead.apply(this, args)
-  }
+    return prevWriteHead.apply(this, args);
+  };
 }
 
 /**
@@ -53,16 +51,16 @@ function createWriteHead (prevWriteHead, listener) {
  * @public
  */
 
-function onHeaders (res, listener) {
+function onHeaders(res, listener) {
   if (!res) {
-    throw new TypeError('argument res is required')
+    throw new TypeError('argument res is required');
   }
 
   if (typeof listener !== 'function') {
-    throw new TypeError('argument listener must be a function')
+    throw new TypeError('argument listener must be a function');
   }
 
-  res.writeHead = createWriteHead(res.writeHead, listener)
+  res.writeHead = createWriteHead(res.writeHead, listener);
 }
 
 /**
@@ -73,9 +71,9 @@ function onHeaders (res, listener) {
  * @private
  */
 
-function setHeadersFromArray (res, headers) {
-  for (var i = 0; i < headers.length; i++) {
-    res.setHeader(headers[i][0], headers[i][1])
+function setHeadersFromArray(res, headers) {
+  for (let i = 0; i < headers.length; i++) {
+    res.setHeader(headers[i][0], headers[i][1]);
   }
 }
 
@@ -87,11 +85,11 @@ function setHeadersFromArray (res, headers) {
  * @private
  */
 
-function setHeadersFromObject (res, headers) {
-  var keys = Object.keys(headers)
-  for (var i = 0; i < keys.length; i++) {
-    var k = keys[i]
-    if (k) res.setHeader(k, headers[k])
+function setHeadersFromObject(res, headers) {
+  const keys = Object.keys(headers);
+  for (let i = 0; i < keys.length; i++) {
+    const k = keys[i];
+    if (k) res.setHeader(k, headers[k]);
   }
 }
 
@@ -102,31 +100,28 @@ function setHeadersFromObject (res, headers) {
  * @private
  */
 
-function setWriteHeadHeaders (statusCode) {
-  var length = arguments.length
-  var headerIndex = length > 1 && typeof arguments[1] === 'string'
-    ? 2
-    : 1
+function setWriteHeadHeaders(statusCode) {
+  const length = arguments.length;
+  const headerIndex = length > 1 && typeof arguments[1] === 'string' ? 2 : 1;
 
-  var headers = length >= headerIndex + 1
-    ? arguments[headerIndex]
-    : undefined
+  const headers =
+    length >= headerIndex + 1 ? arguments[headerIndex] : undefined;
 
-  this.statusCode = statusCode
+  this.statusCode = statusCode;
 
   if (Array.isArray(headers)) {
     // handle array case
-    setHeadersFromArray(this, headers)
+    setHeadersFromArray(this, headers);
   } else if (headers) {
     // handle object case
-    setHeadersFromObject(this, headers)
+    setHeadersFromObject(this, headers);
   }
 
   // copy leading arguments
-  var args = new Array(Math.min(length, headerIndex))
-  for (var i = 0; i < args.length; i++) {
-    args[i] = arguments[i]
+  const args = new Array(Math.min(length, headerIndex));
+  for (let i = 0; i < args.length; i++) {
+    args[i] = arguments[i];
   }
 
-  return args
+  return args;
 }
